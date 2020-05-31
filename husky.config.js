a0be3224c20e607bbdff1d0428dac7680fff0564
Ravi Kallan <ravikallan@MacBook-Pro-van-Ravi.local>
2020-05-31 09:09:29 +0200

    huskey

--- husky.config.js
+++ husky.config.js
@@ -5,7 +5,5 @@ module.exports = {
         "pre-commit": tasks(["npm run format", "npm run lint"]),
         "pre-push": tasks(["npm run format", "npm run lint", "npm run test:huskey"]),
         "commit-msg": "echo $HUSKY_GIT_PARAMS",
-        // 'pre-commit': tasks(['npm run lint', 'npm run test:huskey']),
-        // 'pre-push': tasks(['npm run lint']),
     },
 };

9d55e3c1c7a52f04461d548957338cee5dc67dc6
Ravi Kallan <ravikallan@MacBook-Pro-van-Ravi.local>
2020-05-31 09:20:30 +0200

    huskey comfig

--- husky.config.js
+++ husky.config.js
@@ -3,7 +3,7 @@ const tasks = (arr) => arr.join(" && ");
 module.exports = {
     hooks: {
         "pre-commit": tasks(["npm run format", "npm run lint"]),
-        "pre-push": tasks(["npm run format", "npm run lint", "npm run test:huskey"]),
+        "pre-push": tasks(["npm run format", "npm run lint", "npm run test:once"]),
         "commit-msg": "echo $HUSKY_GIT_PARAMS",
     },
 };

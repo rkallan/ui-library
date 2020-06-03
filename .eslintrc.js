24f526208c51d18a7d4307c74e66b9a47f28717b
Ravi Kallan <ravikallan@MacBook-Pro-van-Ravi.local>
2020-06-03 13:05:53 +0200

    shareble eslint rules and formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,10 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
-        jest: true,
-    },
     extends: [
-        "./node_modules/rkallan-eslint-config",
-        "./node_modules/rkallan-eslint-config-react",
+        "rkallan",
+        "react-rkallan",
         "eslint:recommended",
         "plugin:jsx-a11y/recommended",
         "plugin:react/recommended",
         "plugin:prettier/recommended",
     ],
-    plugins: ["react", "react-hooks", "jsx-a11y", "prettier"],
-    globals: {
-        page: true,
-        browser: true,
-        context: true,
-        jestPuppeteer: true,
-        self: true,
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
-    },
-    parserOptions: {
-        ecmaVersion: 11,
-        sourceType: "module",
-        ecmaFeatures: {
-            jsx: true,
-            globalReturn: true,
-        },
-    },
-    settings: {
-        react: {
-            pragma: "React",
-            version: "16.13",
-        },
-        "import/extensions": [".js", ".jsx"],
-    },
 };
